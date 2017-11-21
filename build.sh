910354f32ffe2664fe1ded1d7e6b283d3426730a
Dale Tristram <dale.tristram@sprinthive.com>
2017-11-21 11:27:38 +0200

    Build binary with static linking

--- build.sh
+++ build.sh
@@ -2,17 +2,20 @@
 
 IMAGE_ID=$(docker images ship-build -q)
 
-if [[ -z "$IMAGE_ID" ]]; then
+if test -z "$IMAGE_ID"; then
   echo "Build image doesn't exist, creating it"
   docker build -t ship-build .
 fi
 
+GOOS=${GOOS:-darwin}
+GOARCH=${GOARCH:-amd64}
+
 echo "Starting build container..."
-docker run --name ship-yard -dt -v "$PWD":/go/src/github.com/SprintHive/ship ship-build cat
+docker run --name ship-yard -e CGO_ENABLED=0 -e GOOS=$GOOS -e GOARCH=$GOARCH -dt -v "$PWD":/go/src/github.com/SprintHive/ship ship-build cat
 echo "Downloading ship dependencies"
 docker exec ship-yard dep ensure -update
 echo "Compiling project"
-docker exec ship-yard go build -o ship .
+docker exec ship-yard go build -a -installsuffix cgo -o ship .
 docker rm -f ship-yard
 
 echo "Done! The binary is called 'ship' in the current directory"
